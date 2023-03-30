bf8c6976bbcb2fb8a58ae65f43b3052d2f896941
Felipe Soeiro <felipe_soeirojl@hotmail.com>
2023-03-30 17:18:38 -0300

    Iniciar seção de contato

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ var typed = new Typed(".mutiple-text", {
     backDelay: 1000,
     loop: true
 })
+
+document.querySelectorAll('.navbar a').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // impede o comportamento padrão do link
+    
+    // obtém o elemento correspondente ao link clicado
+    const target = document.querySelector(link.getAttribute('href'));
+    
+    // rola para o elemento correspondente
+    target.scrollIntoView({ behavior: 'smooth' });
+    
+    // adiciona a classe active no link clicado e remove a classe active dos outros links
+    document.querySelectorAll('.navbar a').forEach(otherLink => {
+      if (otherLink !== link) {
+        otherLink.classList.remove('active');
+      }
+    });
+    link.classList.add('active');
+  });
+});
+
+
+
